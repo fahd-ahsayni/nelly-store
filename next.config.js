db4dc388a20afaafd06e7e2eba598c4e1f467032
Fahd AHSAYNI <fahdahsayni@gmail.com>
2025-05-26 16:06:59 +0200

    test

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["img.freepik.com", "m.media-amazon.com", "cdn.shopify.com", "chateaurouge.net"],
+    domains: [
+      "img.freepik.com",
+      "m.media-amazon.com",
+      "cdn.shopify.com",
+      "chateaurouge.net",
+      "ik.imagekit.io",
+    ],
   },
 };
 
